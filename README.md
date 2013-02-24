3abe513e8c943bc9e90a6a3e608a9a945baedb62
Jure Ham <jure@hamsworld.net>
2013-02-24 22:21:12 +0100

    First version of the script, readme

--- README.md
+++ README.md
@@ -1,4 +1,54 @@
-log-replay
-==========
-
 Benchmark HTTP server by replaying logs
+===
+
+Very simple and straightforward script for messuring http server capabilities by replaying log files.
+
+Usage:
+---
+	usage: replay.py [-h] -a ADDRESS -f FILE -r REQUESTS [-c CONCURRENCY]
+					 [-t TIMEOUT]
+
+	arguments:
+	  -h, --help            show this help message and exit
+	  -a ADDRESS, --address ADDRESS
+							HTTP server address
+	  -f FILE, --file FILE  Log file location
+	  -r REQUESTS, --requests REQUESTS
+							Number of requests
+	  -c CONCURRENCY, --concurrency CONCURRENCY
+							Number of concurrent requests
+	  -t TIMEOUT, --timeout TIMEOUT
+							Request timeout in seconds
+
+	example:
+		python replay.py -a http://localhost:5000 -f access.log -r 10000 -c 4
+
+Report example:
+---
+	requests 10000
+	ok       10000
+	error    0
+
+	Total time: 33.95 sec
+	Requests per second: 295.0
+
+	Response times:
+	mean:	13.5ms
+	10%	8.7ms
+	20%	9.4ms
+	30%	10.0ms
+	40%	10.7ms
+	50%	11.5ms
+	60%	12.5ms
+	70%	13.8ms
+	80%	15.9ms
+	90%	19.3ms
+	100%	619.5ms
+
+Log file format:
+---
+	127.0.0.1 - - [22/Feb/2013:20:15:58 +0000] "GET /path/?parameter=nice HTTP/1.1" 200 26 "http://example.com/referrer" "User Agent String"
+
+Known issues:
+---
+	Supports only GET requests.
---
+++ replay.py
@@ -0,0 +1,197 @@
+import sys
+import time
+import re
+import urllib2
+import numpy
+import argparse
+
+from Queue import Queue, Empty
+from threading import Thread
+
+LOG_LINE_REGEX = r'([0-9\.]+)\s-\s-\s\[.*?\]\s"GET\s(.*?)\sHTTP.*?"\s[0-9]+\s[0-9]+\s".*?"\s"(.*?)"'
+
+class LogParser(object):
+
+	def __init__(self, log_queue, log_file, limit):
+		self.log_file = log_file
+		self.queue = log_queue
+
+		self.log_regex = re.compile(LOG_LINE_REGEX)
+
+		self.limit = limit
+		self.queue_size_max = 1000 if limit > 1000 else limit
+
+		self.queued = 0
+
+	def _get_parsed_line(self, line):
+		m = self.log_regex.match(line)
+		if not m:
+			return None
+		return m.groups()
+
+	def _parse_next_batch(self):
+		lines = self.log_file.readlines(1000000)
+
+		if not lines:
+			return False
+
+		for line in lines:
+			if self.queued >= self.limit:
+				return False
+
+			while self.queue.qsize() > self.queue_size_max:
+				time.sleep(0.1)
+
+			parsed_line = self._get_parsed_line(line)
+			if parsed_line:
+				self.queue.put(parsed_line)
+				self.queued += 1
+		return True
+
+	def _parser_job(self):
+		while self._parse_next_batch(): pass
+
+	def start(self):
+		self.parser_job = Thread(target=self._parser_job)
+		self.parser_job.start()
+
+		while self.queue.qsize() < self.queue_size_max / 2:
+			time.sleep(0.1)
+
+	def stop(self):
+		self.parser_job.join()
+
+
+class RequestWorker(object):
+
+	def __init__(self, log_queue, address, timeout, limit, workers):
+		self.address = address
+		self.timeout = timeout
+
+		self.queue = log_queue
+		self.limit = limit
+		self.workers = workers
+
+		self.print_on = int(limit / 10)
+
+		self.results = {
+			'total': 0,
+			'error': 0,
+			'ok': 0,
+		}
+		self.times = []
+
+	def _print_progress(self):
+		if self.results['total'] % self.print_on == 0:
+			time_total = time.time() - self.t0
+			print 'done', self.results['total'], '/', self.limit, '|', int(round(self.results['ok'] / time_total)), 'per sec'
+
+	def _make_request(self):
+		try:
+			ip, path, user_agent = self.queue.get(False)
+		except Empty:
+			return False
+
+		url = self.address + path
+
+		try:
+			tr0 = time.time()
+
+			request = urllib2.Request(url, headers={"X-RealIP": ip, "User-Agent": user_agent})
+			response = urllib2.urlopen(request, timeout=self.timeout)
+
+			if response.getcode() >= 400:
+				raise Exception("Response code error %s" % response.getcode())
+
+			response.read()
+
+			self.times.append(time.time() - tr0)
+
+			self.results['ok'] += 1
+		except Exception, e:
+			print e
+			self.results['error'] += 1
+
+		self.results['total'] += 1
+
+		self._print_progress()
+
+		return True
+
+	def _log_consumer_job(self):
+		while self._make_request(): pass
+
+	def print_report(self):
+		#TODO: rewrite this method
+		print
+
+		print 'requests %s' % self.results['total']
+		print 'ok       %s' % self.results['ok']
+		print 'error    %s' % self.results['error']
+		print
+
+		print 'Total time: %s sec' % round(self.time_total, 2)
+		print 'Requests per second: %s' % round(self.results['ok'] / self.time_total)
+		print
+
+		def get_ms(f):
+			return round(f, 4) * 1000
+
+		print 'Response times:'
+		print 'mean:\t%sms' % get_ms(numpy.mean(self.times))
+
+		ts = sorted(self.times)
+		tlen = len(self.times)
+		for p in xrange(10, 100, 10):
+			c = float(p) / 100
+			print '%d%%\t%sms' % (c * 100, get_ms(ts[int(tlen * c)]))
+		print '100%%\t%sms' % get_ms(ts[-1])
+
+	def start(self):
+		self.jobs = []
+		for i in xrange(self.workers):
+			co = Thread(target=self._log_consumer_job)
+			self.jobs.append(co)
+			co.start()
+
+		self.t0 = time.time()
+
+	def stop(self):
+		for co in self.jobs:
+			co.join()
+
+		self.time_total = time.time() - self.t0
+
+def parse_args():
+	parser = argparse.ArgumentParser(description='Replay HTTP Benchmark.')
+
+	parser.add_argument('-a', '--address', type=str, help='HTTP server address', required=True)
+	parser.add_argument('-f', '--file', type=str, help='Log file location', required=True)
+	parser.add_argument('-r', '--requests', type=int, help='Number of requests', required=True)
+	parser.add_argument('-c', '--concurrency', type=int, default=1, help='Number of concurrent requests')
+	parser.add_argument('-t', '--timeout', type=int, default=1, help='Request timeout in seconds')
+
+	return parser.parse_args(sys.argv[1:])
+
+def run_benchmark(options):
+	log_queue = Queue()
+
+	log_file = open(options['file'], 'r')
+
+	log_parser = LogParser(log_queue, log_file, options['requests'])
+	request_worker = RequestWorker(log_queue, options['address'], options['timeout'],
+			options['requests'], options['concurrency'])
+
+	log_parser.start()
+
+	request_worker.start()
+	request_worker.stop()
+
+	log_parser.stop()
+
+	request_worker.print_report()
+
+
+if __name__ == '__main__':
+	options = parse_args()
+	run_benchmark(options.__dict__)
